658b89d5159bd60c558901944d8363ab346822b8
Lambertn33 <lambertgithub@gmail.com>
2024-01-11 11:37:35 +0200

    responsivness

--- src/components/destinations/DestinationItem.tsx
+++ src/components/destinations/DestinationItem.tsx
@@ -7,14 +7,16 @@ interface DestinationItemProps {
     price: string;
     days: number;
     location: string;
+    image: string;
   };
-  image: number;
 }
 
-const DestinationItem = ({ destination, image }: DestinationItemProps) => {
+const DestinationItem = ({ destination }: DestinationItemProps) => {
+  // TODO: make image dynamic
+  const imageUrl = `assets/images/destinations/1.png`;
   return (
     <div
-      className={`flex flex-col w-full md:w-[314px] h-[457px] rounded-[24px] relative overflow-hidden bg-no-repeat bg-cover mb-[32px] md:mb-0 bg-[url('assets/images/destinations/${image}.png')] ${styles.item}`}
+      className={`flex flex-col w-full lg:w-[314px] h-[457px] rounded-[24px] relative overflow-hidden bg-no-repeat bg-cover mb-[32px] lg:mb-0 bg-[url('${imageUrl}')] ${styles.item}`}
     >
       <div className="absolute w-full px-[20px] pb-[42px] pt-[28px] bg-white bottom-0">
         <div className="flex flex-col gap-[19px]">
--- src/components/destinations/DestinationsList.tsx
+++ src/components/destinations/DestinationsList.tsx
@@ -5,20 +5,24 @@ interface DestinationInterface {
   price: string;
   days: number;
   location: string;
+  image: string;
 }
 
 const destinations: DestinationInterface[] = [
   {
+    image: "1",
     location: "Rome, Italy",
     days: 10,
     price: "5,42",
   },
   {
+    image: "2",
     location: "London, UK",
     days: 12,
     price: "4,2",
   },
   {
+    image: "3",
     location: "Full Europe",
     days: 28,
     price: "15",
@@ -28,9 +32,9 @@ const destinations: DestinationInterface[] = [
 const DestinationsList = () => {
   return (
     <div className="relative">
-      <div className="grid grid-cols-1 gap-5 sm:gap-20 md:gap-0 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-5 sm:gap-20 md:gap-0 lg:grid-cols-3">
         {destinations.map((destination, index) => 
-          <DestinationItem destination={destination} key={index} image={++index} />
+          <DestinationItem destination={destination} key={index} />
         )}
       </div>
       <div className="absolute right-10 bottom-[47px] -z-10 hidden lg:flex">
