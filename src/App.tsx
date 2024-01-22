933eace3b14b3faf31adddad81203c1dc8cff833
Lambertn33 <lambertgithub@gmail.com>
2024-01-22 07:37:48 +0200

    chore: clean

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,15 @@ import {
   Testimonials,
 } from "@/pages";
 
+import HeroImg from "./assets/images/hero/Hero.png";
+
 const App = () => {
   return (
     <div className="py-11 relative">
       <Hero />
+      <div className="absolute top-0 right-0 -z-10 hidden lg:flex">
+        <img src={HeroImg} alt="" />
+      </div>
       <Services />
       <Destinations />
       <Booking />
